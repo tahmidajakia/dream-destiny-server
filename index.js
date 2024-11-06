571b39326c686f7a4bcc2bf73614ea34221d6588
Mst Tahmida Begum Jakia <devtahmidajakia@gmail.com>
2024-11-06 05:36:48 -0800

    booking deleted method done

--- index.js
+++ index.js
@@ -1,12 +1,17 @@
 const express = require('express');
-const app = express();
 const cors = require('cors');
+const app = express();
 require('dotenv').config()
 const port = process.env.PORT || 5000;
 
 
 // middlewares
-app.use(cors());
+app.use(
+  cors({
+    origin: 'http://localhost:5173', // Allow requests from this origin
+    credentials: true,               // Allow credentials (cookies, etc.)
+  })
+);
 app.use(express.json());
 
 
@@ -31,9 +36,12 @@ async function run() {
 
 
     const hotelCollection = client.db("dreamDestinyDb").collection("hotel");
+    const bookingCollection = client.db('dreamDestinyDb').collection('bookings');
+    const reviewCollection = client.db('dreamDestinyDb').collection('reviews');
+
+
 
     app.get('/hotel', async(req,res) => {
-     
      const result = await hotelCollection.find().toArray();
      res.send(result);
       });
@@ -45,6 +53,86 @@ async function run() {
         const result = await hotelCollection.findOne(query)
         res.send(result)
       });
+
+      app.get('/hotel', async (req, res) => {
+        const { minPrice, maxPrice } = req.query;
+        console.log(req.query);
+        const cursor = hotelCollection.find({
+            price_per_night: {
+                $gte: parseInt(minPrice),
+                $lte: parseInt(maxPrice)
+            }
+        });
+        const result = await cursor.toArray();
+        console.log(result);
+        res.json(result);
+    });
+
+
+
+      app.post('/bookings', async (req, res) => {
+        const booking = req.body;
+        console.log(booking);
+        const result = await bookingCollection.insertOne(booking);
+        res.send(result)
+    })
+
+
+
+      app.get('/bookings', async (req, res) => {
+        const cursor = bookingCollection.find();
+        const result = await cursor.toArray();
+        res.send(result)
+
+    });
+
+
+    // get my booking data by email
+    app.get("/my-bookings/:email", async (req, res) => {
+      const email = req.params.email
+      const query = { email: email }
+      const result = await bookingCollection.find(query).toArray();
+      res.send(result)
+  });
+
+
+   // update date
+   app.patch('/bookings/:id', async (req, res) => {
+    const id = req.params.id;
+    const query = { _id: new ObjectId(id) }
+    const updateDoc = {
+        $set: {
+            checkInDate: req.body.checkInDate,
+            checkOutDate: req.body.checkOutDate
+        }
+    };
+    const result = await bookingCollection.updateOne(query, updateDoc);
+    res.send(result)
+
+});
+
+
+ // cancel booking
+ app.delete('/bookings/:id', async (req, res) => {
+  const id = req.params.id;
+  const query = { _id: new ObjectId(id) };
+  const result = await bookingCollection.deleteOne(query);
+  res.send(result)
+});
+
+
+    app.get('/reviews', async(req,res)=> {
+      const cursor = reviewCollection.find();
+      const result = await cursor.toArray();
+      res.send(result)
+  })
+
+  app.post('/reviews', async(req,res)=> {
+      const review = req.body;
+      console.log(review);
+      const result = await reviewCollection.insertOne(review);
+      res.send(result)
+  });
 
 
 
